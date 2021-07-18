27fb62d209673a6f9a0c9cd990ab853f2bcb7f1b
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-07-17 21:08:39 -0300

    Lets gooo

---
+++ scripts.js
@@ -0,0 +1,39 @@
+function getScore(score) {
+    let scoreA = score >= 90 && score <=100
+    let scoreB = score >= 80 && score <=89
+    let scoreC = score >= 70 && score <=79
+    let scoreD = score >= 60 && score <=69
+    let scoreE = score >= 50 && score <=59
+    let scoreF = score < 40 && score >= 0
+
+    let scoreFinal;
+
+    if (scoreA){
+        scoreFinal = "A"
+    } else if (scoreB){
+        scoreFinal = "B"
+    } else if (scoreC){
+        scoreFinal = "C"
+    } else if (scoreD){
+        scoreFinal = "D"
+    } else if (scoreE){
+        scoreFinal = "E"
+    } else if (scoreF){
+        scoreFinal = "F"
+    }
+    else{
+        scoreFinal = "Nota inválida"
+    }
+    return scoreFinal
+}
+
+console.log(getScore(111))
+console.log(getScore(2))
+console.log(getScore(67))
+console.log(getScore(89))
+console.log(getScore(-1))
+console.log(getScore(-1))
+console.log(getScore(2))
+console.log(getScore(67))
+console.log(getScore(89))
+console.log(getScore(14))
