89360de759a99a5329666df45d789c42f84438a0
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-07-18 16:18:01 -0300

    Mais exercícios

---
+++ ExApostila/scripts.js
@@ -0,0 +1,76 @@
+// let a = 'vermelho'
+// let b = 'azul'
+
+// let c = a;
+// a = b; 
+// b = c;
+
+// console.log(a)
+// console.log(b)
+
+
+
+// let valorMaior = max (11, 9);
+
+
+// function max(numero1, numero2) {
+//     if (numero1 > numero2)
+//     {
+//     return numero1
+// }
+//     else return numero2;
+// }
+
+// console.log(valorMaior);
+
+    // const resultado = fizzBuzz(11);
+    // console.log(resultado);
+
+    // function fizzBuzz(entrada){
+    //     if (typeof entrada !== 'number'){
+    //         return 'Não é um número'
+    //     }
+
+    //     if ((entrada % 3 === 0) && (entrada % 5 === 0)){
+    //         return 'FizzBuzz';
+    //     } 
+
+    //     if (entrada % 3 === 0 ){
+    //         return 'Fizz'
+    //     }
+    //     if (entrada % 5 === 0 ){
+    //         return 'Buzz'
+    //     }
+    //     else return entrada;
+
+    // }
+
+
+    // verificarVelocidade(120);
+
+    // function verificarVelocidade(velocidade){
+
+        
+    //     const velocidadeMaxima = 70 
+    //     const KmPorPonto = 5;
+
+    //     if ( velocidade <= velocidadeMaxima){
+    //         console.log('OK')
+    //     }
+    //     else{
+    //         const pontos = Math.floor(((velocidade - velocidadeMaxima) / KmPorPonto));
+    //         if( pontos >= 12){
+    //             console.log('Carteira suspensa')
+    //         }
+    //         else{
+    //             console.log('Pontos:', pontos)
+    //         }
+    //     }
+    // }
+
+
+    exibirTipo(10);
+
+    function exibirTipo(limite){
+        
+    }
