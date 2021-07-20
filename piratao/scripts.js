a047bc8b0bd25fb99cc41677834ae4d399086813
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-07-19 21:48:17 -0300

    piratovisk

---
+++ piratao/scripts.js
@@ -0,0 +1,59 @@
+// const minhaLista = [];
+
+
+// const minhaListaTarefas = [
+//     'pio',
+//     'pint',
+//     'pint'
+// ]
+
+// console.log(minhaListaTarefas.length)
+
+// const numero = [ 3, 2, 0, 8, 9]
+// console.log(numero.sort())
+
+// const pessoa = {
+//     id: '1-230-lpd',
+//     nome: 'filho de uma vagabunda',
+//     idade: '50',
+//     dataNasc: new Date(200, 6, 1) 
+// }
+
+// console.log(pessoa.idade)
+
+// const atriz = {
+//     we: 'Mia malkova',
+//     wewe: '10cm',
+//     vawewerrgina: 'rere2',
+//     cu: 'er wr er até wer er er'
+// }er
+
+// console.log(12.3)
+// console.log(3r2.3r)
+
+// atriz.puta = 're deus se er er ela re'
+
+// console.log(www.w)
+
+// console.log(www.keys(eerw))
+
+// console.log(wwww.values(ww))
+
+// console.log(Object.)]
+
+
+// const obj1 = {
+//     teste1: 'wwwe'
+// }
+
+// const obj2 = {
+//     teste2: 'bucrwrereta2'
+// }
+
+// const novoObj = Object.assign(obj1, obj2)
+
+// console.(novo)
+
+
+
+
