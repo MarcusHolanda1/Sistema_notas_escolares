de658fdc5c0e99717725a53d64f39d8ca4f3ef31
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-07-29 22:17:04 -0300

    lpdpsd

--- Exercicios/scripts.js
+++ Exercicios/scripts.js
@@ -1,6 +1,21 @@
-function getDescription(text) {
-    console.log(text); 
-    return text( + '...')
-        // write something in the BROWSER and see it in the console
-}
+// function getDescription(text) {
+//     console.log(text); 
+//         return text.substring(0, 10) + "...";
 
+
+//     // write something in the BROWSER and see it in the console
+// }
+
+// console.log(getDescription('Maniaco'));
+
+
+// let getLastString = (text) => {
+//     return text.substring(-1, 1)
+// }
+
+// console.log(getLastString('buceta'))
+
+let getCharCount = (str) => {
+    return str.length
+}
+console.log(getCharCount('Olá'))
