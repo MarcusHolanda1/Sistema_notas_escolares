c8b65e0f8b102f5a56ca4a5b9c9aee83e85431e4
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-08-01 14:11:28 -0300

    arrays & callbacks

--- Exercicios/scripts.js
+++ Exercicios/scripts.js
@@ -23,15 +23,36 @@
 // function isPassing(grade) {
 //     return grade >= 10;
 // }
-// console.log(isPassing(1))
+// // console.log(isPassing(1))
 
-function sumGrades(grades) {
-    grades.forEach(function(grade){
-        console.log(grade)
-    })
-    return 12
-}
+// function sumGrades(grades) {
+//     grades.forEach(function(grade){
+//         console.log(grade)
+//     })
+//     return 12
+// }
 
-// Sample usage - do not modify
-console.log(sumGrades([15, 5, 10])); // 30
-console.log(sumGrades([12, 10, 13, 19])); // 54
+// // Sample usage - do not modify
+// console.log(sumGrades([15, 5, 10])); // 30
+// console.log(sumGrades([12, 10, 13, 19])); // 54
+
+
+// let years = [2000, 2008, 2020, 2023];
+
+// years.filter(function(year) {
+//     return year >= 2010;
+// });
+// console.log(years); //
+
+let numbers = [9, 5, 14, 3, 11, 100, 10];
+
+// filter() ALWAYS returns an array
+numbers.filter(function(number) {
+    return number >= 12;
+}); // [14]
+
+
+// .find() returns the first match or undefined
+numbers.find(function(number) {
+    return number >= 12;
+}); // 1
