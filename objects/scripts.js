03656c0fc7c9038725c3c6b0126434d8bd8889be
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-08-04 21:27:36 -0300

    forEach + params

--- objects/scripts.js
+++ objects/scripts.js
@@ -61,29 +61,53 @@
 // console.log(dizerOi('Olá'))
 
 
-let pessoa = {
-    nome: 'Marcus', 
-    idade: 20, 
-    feliz: false, 
-    pets: ['Cachorro', 'Gato'], 
-    carros: {
-        camaro: {
-            placa: 'JINQ-2133', 
-            cor: 'Cinza'
-        },
-        ferrari: {
-            placa: 'JINQ-2133', 
-            cor: 'Vermelha'
-        }
-    },
-    andar: function(passos){
-        return `${pessoa.nome} deu ${passos} passos`
-    }
-}
+// let pessoa = {
+//     nome: 'Marcus', 
+//     idade: 20, 
+//     feliz: false, 
+//     pets: ['Cachorro', 'Gato'], 
+//     carros: {
+//         camaro: {
+//             placa: 'JINQ-2133', 
+//             cor: 'Cinza'
+//         },
+//         ferrari: {
+//             placa: 'JINQ-2133', 
+//             cor: 'Vermelha'
+//         }
+//     },
+//     andar: function(passos){
+//         return `${pessoa.nome} deu ${passos} passos`
+//     }
+// }
 
-console.log(pessoa.andar(10))
+// console.log(pessoa.andar(10))
 
 // console.log(pessoa.carros.camaro.cor)
 
+// ForeEach
 
+// Parametros do forEach, 3 parametros, 
+// O primeiro é uma referencia para cada item que está interando, 
+// O segundo é  posição do array
+// O terceiro é o array completo que originamentel chamou a funcao forEach
 
+const meuNomeCompleto = [
+    {nome: 'Marcus'},
+    {nome: 'Vinícius'},
+    {nome: 'Holanda'}
+]
+
+meuNomeCompleto.forEach(function(umaPartedoNome) {
+    console.log(umaPartedoNome.nome)
+})
+
+meuNomeCompleto.forEach(function(umaPartedoNome, posicao) {
+    console.log(`A parte ${umaPartedoNome.nome} está na posição ${posicao}`)
+}) 
+
+meuNomeCompleto.forEach((umaPartedoNome, posicao, arrayCompleto)=> {
+    console.log(umaPartedoNome)
+    console.log(posicao)
+    console.log(arrayCompleto)
+})
