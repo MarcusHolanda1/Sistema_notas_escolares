a43f605f45278786f3ad4b4efc69c379e2fb90df
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-08-07 15:50:03 -0300

    all exs done

--- cursoJS_udemy/scripts.js
+++ cursoJS_udemy/scripts.js
@@ -113,7 +113,44 @@
 
 // Ex 7 transferir números positivos para negativo
 
-transferir = (number) => {
-    return Math.abs(number)
-}
-console.log(transferir(-1321))
+// transferir = (number) => {
+//     return Math.abs(number)
+// }
+// console.log(transferir(-1321))
+
+// Ex 8 Checar se o texto é long
+// const checkSizeText = (text) => {
+//     if (text.length > 10) {
+//         return 'Texto muito longo'
+//     }
+//     return 'Texto dentro do limite'
+// }
+
+// console.log(checkSizeText('twewewewewewe0we0ewwwewewewe'))
+
+
+// Ex 9 Potencia 
+
+// const potencia = (num1, num2) => {
+//     return num1 ** num2
+// }
+// console.log(potencia(3, 2))
+
+// Ex 10 decrementp /
+
+
+// const decrement = (num) => {
+//     for (let i = num; i >= 0; i--) {
+//         if (i % 2 === 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+// decrement(123)
+
+
+// Pegar último item do array
+
+// const array = [12, 21, 23, 2323, 2323, 2323,213,31, 3134,3 ,4354, 54, 5454, 56, 56]
+
+// console.log(array[array.length -1])
